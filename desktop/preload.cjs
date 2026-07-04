248b5f95255632bb058f15a4ab646a7e0bf525f2
A-engi <cazvdg@live.co.uk>
2026-07-04 17:14:35 +0100

    Remove desktop viewer from web repo

--- desktop/preload.cjs
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("FieldOpsDesktop", {
-  getSource: () => ipcRenderer.invoke("fieldops:source"),
-  useLiveSource: () => ipcRenderer.invoke("fieldops:set-source", "live"),
-  useLocalSource: () => ipcRenderer.invoke("fieldops:set-source", "local")
-});
